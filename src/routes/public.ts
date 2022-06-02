a22f7b8004d1514c4a760aa4d6e5114456c40048
fmidyt <fmidyt@gmail.com>
2022-06-03 00:29:41 +0700

    feat(routes): add public routes

---
+++ src/routes/public.ts
@@ -0,0 +1,32 @@
+import Express, { Request, Response } from 'express'
+import { BASE_URL_SERVER } from '../constants/ConstBaseURL'
+import { NODE_ENV } from '../utils/env'
+
+const route = Express.Router()
+
+interface IndexResponseData {
+  message: string
+  maintainer: string
+  source: string
+  docs?: string
+}
+
+// index route
+route.get('/', function(req: Request, res: Response) {
+  let data: IndexResponseData = {
+    message: 'Welcome to Fahmidyt Backend API Service!',
+    maintainer: 'fahmidyt, <fmidyt@gmail.com> <https://github.com/fahmidyt>',
+    source: 'https://github.com/fahmidyt/fahmidyt-be/',
+  }
+
+  if (NODE_ENV !== 'production') {
+    data = {
+      ...data,
+      docs: `${BASE_URL_SERVER}/v1/api-docs`,
+    }
+  }
+
+  return res.status(200).json(data)
+})
+
+export default route
