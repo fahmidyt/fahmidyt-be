0a9fcd98a07bc1fb091669e38dc425f7e0c71af0
fmidyt <fmidyt@gmail.com>
2022-07-19 23:12:13 +0700

    feat: wording utilities

--- src/utils/__test__/wording.test.ts
+++ src/utils/__test__/wording.test.ts
@@ -1,24 +1,31 @@
 import wording from '@utils/wording'
 
-describe('Utils: Wording', () => {
+describe('[Utils]: Wording', () => {
   // wording required
   it("should wording 'required' properly", () => {
-    const word: string = wording.required("property")
+    const word: string = wording.required('property')
 
     expect(word).toMatch('property is required')
   })
 
   // wording minLength
   it("should wording 'minLength' properly", () => {
-    const word: string = wording.minLength("property", 12)
+    const word: string = wording.minLength('property', 12)
 
     expect(word).toMatch('property min length is 12')
   })
 
   // wording maxLength
   it("should wording 'maxLength' properly", () => {
-    const word: string = wording.maxLength("property", 24)
+    const word: string = wording.maxLength('property', 24)
 
     expect(word).toMatch('property max length is 24')
   })
- })
+
+  // wording maxLength
+  it("should wording 'maxLength' properly", () => {
+    const word: string = wording.match('property', 'abc')
+
+    expect(word).toMatch('property must match with abc')
+  })
+})
--- src/utils/wording.ts
+++ src/utils/wording.ts
@@ -1,5 +1,9 @@
 export default {
   required: (property: string) => `${property} is required`,
-  minLength: (property: string, len: number) => `${property} min length is ${len}`,
-  maxLength: (property: string, len: number) => `${property} max length is ${len}`,
+  minLength: (property: string, len: number) =>
+    `${property} min length is ${len}`,
+  maxLength: (property: string, len: number) =>
+    `${property} max length is ${len}`,
+  match: (property: string, match: string) =>
+    `${property} must match with ${match}`,
 }
