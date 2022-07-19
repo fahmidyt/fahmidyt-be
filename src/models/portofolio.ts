0ca83528740a7ea3ce11ddf776eff9375eb107f1
fmidyt <fmidyt@gmail.com>
2022-07-19 23:13:12 +0700

    feat(model): add portofolio

--- src/models/portofolio.ts
+++ src/models/portofolio.ts
@@ -1,23 +1,33 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const { Schema } = mongoose;
+const { Schema } = mongoose
 
 // model interface
 export interface IPortofolio {
-  name: string;
-  type: string;
-  image: string;
-  description: string;
+  name: string
+  type: string
+  image: string
+  description: string
+}
+
+export interface PortofolioDocument extends IPortofolio, mongoose.Document {
+  createdAt: Date
+  updatedAt: Date
 }
 
 // model schema
-const PortofolioSchema = new Schema<IPortofolio>({
+const PortofolioSchema = new Schema<PortofolioDocument>({
   name: { type: String, required: true },
   type: { type: String, required: true },
   image: { type: String, required: true },
   description: { type: String, required: true },
-});
+})
 
-const Portofolio = mongoose.model<IPortofolio>("Portofolio", PortofolioSchema);
+PortofolioSchema.index({ name: 1 })
 
-export default Portofolio;
+const Portofolio = mongoose.model<PortofolioDocument>(
+  'Portofolio',
+  PortofolioSchema
+)
+
+export default Portofolio
