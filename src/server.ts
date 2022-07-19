0daa96e5bf98314bd83a84dee449890add4102bc
fmidyt <fmidyt@gmail.com>
2022-07-19 23:08:41 +0700

    refactor: while init database

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
-import App from "app";
-import InitDatabase from "@config/database";
+import './moduleAlias'
+import App from './app'
+import InitDatabase from '@config/database'
 
-const Server = new App();
+const Server = new App()
 
-InitDatabase();
+InitDatabase()
+  .then(() => console.log('Connected to Database'))
+  .catch((err: any) => {
+    console.error(err)
+  })
 
-Server.run();
+Server.run()
