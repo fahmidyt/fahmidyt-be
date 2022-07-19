feed61f9a1f4443dae26081ce35f11647d65b78b
fmidyt <fmidyt@gmail.com>
2022-07-19 23:09:09 +0700

    refactor: database config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 import {
   MONGODB_AUTH,
   MONGODB_DATABASE,
@@ -6,7 +6,7 @@ import {
   MONGODB_PASSWORD,
   MONGODB_PORT,
   MONGODB_USERNAME,
-} from "@utils/env";
+} from '@utils/env'
 
 const URI: string = `mongodb://${MONGODB_HOST}:${MONGODB_PORT}/${MONGODB_DATABASE}`
 const OPT: mongoose.ConnectOptions = {
@@ -15,10 +15,18 @@ const OPT: mongoose.ConnectOptions = {
   authSource: MONGODB_AUTH,
 }
 
-const InitDatabase =  async (): Promise<void> => {
+export const DBCallback = (err?: mongoose.CallbackError): void => {
+  if (err) {
+    console.error(err.message)
+    return
+  }
+
+  console.log(`Successfully connected to database: ${MONGODB_DATABASE}`)
+}
+
+const InitDatabase = async (): Promise<void> => {
   try {
-    await mongoose.connect(URI, OPT)
-    console.log(`Successfully connected to database: ${MONGODB_DATABASE}`)
+    await mongoose.connect(URI, OPT, DBCallback)
   } catch (err) {
     console.error(`DATABASE ERROR: `, err)
 
